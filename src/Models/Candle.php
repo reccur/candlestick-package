4927667abaae2e2aa9e4330598aa1e685e043e6d
Nishan Hitang <nishanhitang@gmail.com>
2022-07-10 16:28:53 +0545

    Optimized Single Pattern Algorithm

--- src/Models/Candle.php
+++ src/Models/Candle.php
@@ -2,208 +2,215 @@
 
 namespace Reccur\Candlestick\Models;
 
+class Patterns{
+    public $base;
+    public $specific;
+}
+
 class Candle{
 
-        public $date;
-        public $o;
-        public $h;
-        public $l;
-        public $c;
-        public $volume;
+    public $date;
+    public $o;
+    public $h;
+    public $l;
+    public $c;
+    public $volume;
 
-        public $color;
+    public $color;
 
-        public $candle;
-        public $upper_wick;
-        public $body;
-        public $lower_wick;
-        public $wicks;
+    public $candle;
+    public $upper_wick;
+    public $body;
+    public $lower_wick;
+    public $wicks;
 
-        public $pattern;
+    public $pattern_base;
+    public $pattern_specific;
+    
+    public function __construct($params){
+
+        $this->date = (string) $params['date'] ?? '';
+        $this->o = (float) $params['o'] ?? 0;
+        $this->h = (float) $params['h'] ?? 0;
+        $this->l = (float) $params['l'] ?? 0;
+        $this->c = (float) $params['c'] ?? 0;
+        $this->volume = (float) $params['volume'] ?? 0;
+
+        $this->setColor();
+        $this->setStructure();
+        $this->setPatterns();
         
-        public function __construct($params){
+    }
 
-            $this->date = (string) $params['date'] ?? '';
-            $this->o = (float) $params['o'] ?? 0;
-            $this->h = (float) $params['h'] ?? 0;
-            $this->l = (float) $params['l'] ?? 0;
-            $this->c = (float) $params['c'] ?? 0;
-            $this->volume = (float) $params['volume'] ?? 0;
+    /* Getters */
 
-            $this->setColor();
-            $this->setStructure();
-            $this->setPattern();
-            
+    public function date(){
+        return $this->date;
+    }
+
+    public function o() : float{
+        return $this->o;
+    }
+
+    public function h() : float{
+        return $this->h;
+    }
+
+    public function l() : float{
+        return $this->l;
+    }
+
+    public function c() : float{
+        return $this->c;
+    }
+
+    public function volume() : float{
+        return $this->volume;
+    }
+
+    public function color(){
+        return $this->color;
+    }
+
+    public function candle(){
+        return $this->candle;
+    }
+
+    public function upper_wick(){
+        return $this->upper_wick;
+    }
+
+    public function body(){
+        return $this->body;
+    }
+
+    public function lower_wick(){
+        return $this->lower_wick;
+    }
+
+    public function wicks(){
+        return $this->wicks;
+    }
+
+    public function pattern_base(){
+        return $this->pattern_base;
+    }
+
+    public function pattern_specific(){
+        return $this->pattern_specific;
+    }
+
+    /* Getters */
+
+
+    // setter for color
+    public function setColor(){
+        if($this->c == $this->o){
+            $this->color = "TRUE_DOJI";
+        } else {
+            $this->color = $this->c > $this->o ? "GREEN":"RED";
+        }
+        return $this;
+    }
+
+    // setter for structure
+    public function setStructure(){
+        $upper_wick = $this->color=="GREEN"?($this->h - $this->c):($this->h - $this->o);
+        $body = abs($this->o - $this->c);
+        $lower_wick = $this->color=="GREEN"?($this->o - $this->l):($this->c - $this->l);
+
+        $total_length = $upper_wick + $body + $lower_wick;
+
+        $this->candle = 100;
+        $this->upper_wick = round($upper_wick / $total_length * 100, 4);
+        $this->body = round($body / $total_length * 100, 4);
+        $this->lower_wick = round($lower_wick / $total_length * 100, 4);
+        $this->wicks = round(100 - $this->body, 4);
+    }
+
+    public function setPatterns(){
+
+        // DOJI : 20 * ABS(O - C) <= H - L
+        if( (config('candlestick.DOJI_WICK_MULTIPLIER') * abs($this->o - $this->c)) <= ($this->h - $this->l) ){
+            $this->pattern_base = "DOJI";
         }
 
-        /* Getters */
-
-        public function date(){
-            return $this->date;
+        // DOJI_DRAGONFLY
+        if(
+            ($this->lower_wick > config('candlestick.DOJI_WICK_MULTIPLIER') * $this->body) && 
+            ($this->upper_wick <= config('candlestick.DOJI_SHORTER_WICK_THRESHOLD'))
+        ){
+            $this->pattern_specific = "DRAGONFLY";
         }
 
-        public function o() : float{
-            return $this->o;
+        // DOJI_GRAVESTONE
+        if(
+            ($this->upper_wick > config('candlestick.DOJI_WICK_MULTIPLIER') * $this->body) && 
+            ($this->lower_wick <= config('candlestick.DOJI_SHORTER_WICK_THRESHOLD'))
+        ){
+            $this->pattern_specific = "GRAVESTONE";
         }
 
-        public function h() : float{
-            return $this->h;
+        // MARUBOZU : Must be a long candle without any wicks
+        if(
+            ($this->body == 100) && 
+            (config('candlestick.MARUBOZU_MULTIPLIER') * abs($this->o - $this->c) >= (($this->o + $this->c)) / 2)
+        ){
+            $this->pattern_base = "MARUBOZU";
+            $this->pattern_specific = "FULL";
         }
 
-        public function l() : float{
-            return $this->l;
+        // CLOSING_MARUBOZU : (L = C OR C = H) AND H - L > ABS(O - C)
+        if(
+            ($this->l == $this->c || $this->c == $this->h) &&
+            $this->h - $this->l > abs($this->o - $this->c) &&
+            $this->wicks > 0
+        ){
+            $this->pattern_base = "MARUBOZU";
+            $this->pattern_specific = "CLOSING";
         }
 
-        public function c() : float{
-            return $this->c;
+        // OPENING_MARUBOZU : (L = O OR O = H) AND H - L > ABS(O - C)
+        if(
+            ($this->l == $this->o || $this->o == $this->h) && 
+            $this->h - $this->l > abs($this->o - $this->c) &&
+            $this->wicks > 0
+        ){
+            $this->pattern_base = "MARUBOZU";
+            $this->pattern_specific = "OPENING";
         }
 
-        public function volume() : float{
-            return $this->volume;
+        // HAMMER_OR_HANGING_MAN
+        if(
+            ($this->lower_wick > config('candlestick.HANGING_MAN_LONGER_WICK_THRESHOLD')) && 
+            ($this->upper_wick < config('candlestick.HANGING_MAN_SHORTER_WICK_THRESHOLD'))
+        ){
+            $this->pattern_base = "HANGING_MAN";
         }
 
-        public function color(){
-            return $this->color;
+        // // INVERTED_HAMMER_OR_SHOOTING_STAR
+        if(
+            ($this->upper_wick > config('candlestick.SHOOTING_STAR_LONGER_WICK_THRESHOLD')) && 
+            ($this->lower_wick < config('candlestick.SHOOTING_STAR_LONGER_WICK_THRESHOLD'))
+        ){
+            $this->pattern_base = "SHOOTING_STAR";
         }
 
-        public function candle(){
-            return $this->candle;
-        }
-
-        public function upper_wick(){
-            return $this->upper_wick;
-        }
-
-        public function body(){
-            return $this->body;
-        }
-
-        public function lower_wick(){
-            return $this->lower_wick;
-        }
-
-        public function wicks(){
-            return $this->wicks;
-        }
-
-        public function pattern(){
-            return $this->pattern;
-        }
-
-        /* Getters */
-
-        
-
-        // setter for color
-        public function setColor(){
-            if($this->c == $this->o){
-                $this->color = "TRUE_DOJI";
+        // SPINNING_TOP
+        if(
+            ($this->body < config('candlestick.SPINNING_TOP_BODY_THRESHOLD')) && 
+            abs($this->upper_wick - $this->lower_wick) < config('candlestick.SPINNING_TOP_WICKS_DIFFERENCE_THRESHOLD')
+        ){
+            $this->pattern_base = "SPINNING_TOP";
+            if($this->upper_wick == $this->lower_wick){
+                $this->pattern_specific = "CENTERED";
             } else {
-                $this->color = $this->c > $this->o ? "GREEN":"RED";
-            }
-            return $this;
-        }
-
-        public function setStructure(){
-            $upper_wick = $this->color=="GREEN"?($this->h - $this->c):($this->h - $this->o);
-            $body = abs($this->o - $this->c);
-            $lower_wick = $this->color=="GREEN"?($this->o - $this->l):($this->c - $this->l);
-
-            $total_length = $upper_wick + $body + $lower_wick;
-
-            $this->candle = 100;
-            $this->upper_wick = round($upper_wick / $total_length * 100);
-            $this->body = round($body / $total_length * 100);
-            $this->lower_wick = round($lower_wick / $total_length * 100);
-            $this->wicks = 100 - $this->body;
-        }
-
-        public function stats(){
- 
-            if( $this->pattern == "MARUBOZU" ){
-
-                echo "date : {$this->date}<br>";
-                echo "color : {$this->color}<br>";
-                echo "pattern : {$this->pattern}<br>";
-                echo "upper_wick : {$this->upper_wick}<br>";
-                echo "body : {$this->body}<br>";
-                echo "lower_wick : {$this->lower_wick}<br>";
-                echo "<br>";
+                $this->pattern_specific = "OFFSET";
             }
         }
 
-        public function setPattern(){
-
-            // DOJI : 20 * ABS(O - C) <= H - L
-            if( (config('candlestick.DOJI_WICK_MULTIPLIER') * abs($this->o - $this->c)) <= ($this->h - $this->l) ){
-                $this->pattern = "DOJI";
-            }
-
-            // MARUBOZU : ABS(H - L) = ABS(O - C)
-            if( $this->body == 100 ){
-                $this->pattern = "MARUBOZU";
-            }
-
-            // CLOSING_MARUBOZU : (L = C OR C = H) AND H - L > ABS(O - C)
-            if(
-                ($this->l == $this->c || $this->c == $this->h) &&
-                $this->h - $this->l > abs($this->o - $this->c) &&
-                $this->wicks < config('candlestick.MARUBOZU_WICKS_THRESHOLD')
-            ){
-                $this->pattern = "CLOSING_MARUBOZU";
-            }
-
-            // OPENING_MARUBOZU : (L = O OR O = H) AND H - L > ABS(O - C)
-            if(
-                ($this->l == $this->o || $this->o == $this->h) && 
-                $this->h - $this->l > abs($this->o - $this->c) &&
-                $this->wicks < config('candlestick.MARUBOZU_WICKS_THRESHOLD')
-            ){
-                $this->pattern = "OPENING_MARUBOZU";
-            }
-
-            // HAMMER_OR_HANGING_MAN
-            if(
-                ($this->lower_wick > $this->body * config('candlestick.HAMMER_LONGER_WICK_MULTIPLIER')) && 
-                ($this->upper_wick < config('candlestick.HAMMER_SHORTER_WICK_THRESHOLD'))
-            ){
-                $this->pattern = "HAMMER_OR_HANGING_MAN";
-            }
-
-            // INVERTED_HAMMER_OR_SHOOTING_STAR
-            if(
-                ($this->upper_wick > $this->body * config('candlestick.HAMMER_LONGER_WICK_MULTIPLIER')) && 
-                ($this->lower_wick < config('candlestick.HAMMER_SHORTER_WICK_THRESHOLD'))
-            ){
-                $this->pattern = "INVERTED_HAMMER_OR_SHOOTING_STAR";
-            }
-
-            // DRAGONFLY_DOJI
-            if(
-                ($this->lower_wick > config('candlestick.DOJI_WICK_MULTIPLIER') * $this->body) && 
-                ($this->upper_wick < config('candlestick.DOJI_SHORTER_WICK_THRESHOLD'))
-            ){
-                $this->pattern = "DRAGONFLY_DOJI";
-            }
-
-            // GRAVESTONE_DOJI
-            if(
-                ($this->upper_wick > config('candlestick.DOJI_WICK_MULTIPLIER') * $this->body) && 
-                ($this->lower_wick < config('candlestick.DOJI_SHORTER_WICK_THRESHOLD'))
-            ){
-                $this->pattern = "GRAVESTONE_DOJI";
-            }
-
-            // SPINNING_TOP
-            if(
-                ($this->body * config('candlestick.SPINNING_TOP_WICKS_MULTIPLIER') < (100 - $this->body)) && 
-                abs($this->upper_wick - $this->lower_wick) < config('candlestick.SPINNING_TOP_WICKS_DIFFERENCE_THRESHOLD')
-            ){
-                $this->pattern = "SPINNING_TOP";
-            }
-
-
-        }
 
     }
- ?>
+
+}
+?>
